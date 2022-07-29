f60a94ec2acde3054d05666f2c62771f1d615c17
SimpleArt <nguyej17@my.erau.edu>
2022-07-28 23:23:00 -0700

    Added priority queues

--- advanced_collections/queue/__init__.py
+++ advanced_collections/queue/__init__.py
@@ -2,6 +2,16 @@ from ._src.abc_queue import AbstractQueue
 from ._src.deque import Deque
 from ._src.fifo import FifoQueue
 from ._src.lifo import LifoQueue
+from ._src.max_priority import MaxPriorityQueue
+from ._src.min_priority import MinPriorityQueue
 from ._src.queue_protocol import QueueProtocol
 
-__all__ = ["AbstractQueue", "Deque", "FifoQueue", "LifoQueue", "QueueProtocol"]
+__all__ = [
+    "AbstractQueue",
+    "Deque",
+    "FifoQueue",
+    "LifoQueue",
+    "MaxPriorityQueue",
+    "MinPriorityQueue",
+    "QueueProtocol",
+]
